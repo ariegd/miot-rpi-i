1e94dde2e448b35dc319921cdc86f0aa3485cad2
zodd <zodd@nootbster.home>
2025-09-30 15:17:21 +0200

    Solución de errores

--- src/lab1/main/lab1.c
+++ src/lab1/main/lab1.c
@@ -3,7 +3,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "esp_chip_info.h"   // <-- chip info está aquí ahora
+#include "esp_flash.h"
 
 void app_main(void)
 {
@@ -20,10 +21,16 @@ void app_main(void)
 
     printf("silicon revision %d, ", chip_info.revision);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    // Flash size
+    uint32_t size_flash_chip;
+    if (esp_flash_get_size(NULL, &size_flash_chip) == ESP_OK) {
+        printf("Flash size: %luMB\n", size_flash_chip / (1024 * 1024));
+    } else {
+        printf("Failed to get flash size\n");
+    }
 
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+    printf("Minimum free heap size: %lu bytes\n",
+           (unsigned long) esp_get_minimum_free_heap_size());
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
