7bfa2b7dc61daff9ceebe1788c7dab74badd872b
zodd <zodd@nootbster.home>
2025-10-01 15:47:41 +0200

    Personalización del proyecto

--- src/lab1/main/lab1.c
+++ src/lab1/main/lab1.c
@@ -6,8 +6,14 @@
 #include "esp_chip_info.h"   // <-- chip info está aquí ahora
 #include "esp_flash.h"
 #include "esp_log.h"
-
 #include "esp_wifi.h"       // <--info de WiFi
+
+// #define CONFIG_HELLO_TASK_DELAY 1000
+// #define CONFIG_WIFI_TASK_DELAY 2000
+
+static const char *TAG_HELLO = "HELLO_TASK";
+static const char *TAG_WIFI  = "WIFI_TASK";
+
 
 void print_wifi_info() {
     wifi_ap_record_t ap_info;
@@ -22,61 +28,57 @@ void print_wifi_info() {
 
 void hello_task(void *pvParameter)
 {
-    printf(" [HELLO] Hello world!\n");
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("[HELLO] This is %s chip with %d CPU cores, WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+      esp_chip_info_t chip_info;
+      esp_chip_info(&chip_info);
 
-    printf("[HELLO] silicon revision %d, ", chip_info.revision);
+      while (1) {
+        printf(" [HELLO] Hello world!\n");   // se puede reemplazar por ESP_LOGI(TAG_HELLO, " Hello world!")
+        
+        /* Print chip information */
 
-    // Flash size
-    uint32_t size_flash_chip;
-    if (esp_flash_get_size(NULL, &size_flash_chip) == ESP_OK) {
-        printf("[HELLO] Flash size: %luMB\n", size_flash_chip / (1024 * 1024));
-    } else {
-        printf("[HELLO] Failed to get flash size\n");
+        printf("[HELLO] This is %s chip with %d CPU cores, WiFi%s%s, ",
+                CONFIG_IDF_TARGET,
+                chip_info.cores,
+                (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+                (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+        printf("[HELLO] silicon revision %d, ", chip_info.revision);
+
+        // Flash size
+        uint32_t size_flash_chip;
+        if (esp_flash_get_size(NULL, &size_flash_chip) == ESP_OK) {
+            printf("[HELLO] Flash size: %luMB\n", size_flash_chip / (1024 * 1024));
+        } else {
+            printf("[HELLO] Failed to get flash size\n");
+        }
+
+        printf("[HELLO] Minimum free heap size: %lu bytes\n",
+               (unsigned long) esp_get_minimum_free_heap_size());  
+           
+        vTaskDelay(CONFIG_HELLO_TASK_DELAY / portTICK_PERIOD_MS);
     }
-
-    printf("[HELLO] Minimum free heap size: %lu bytes\n",
-           (unsigned long) esp_get_minimum_free_heap_size());  
-    
-    for (int i = 10; i >= 0; i--) {
-        printf("[HELLO] Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("[HELLO] Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
 
 void wifi_info_task(void *pvParameter)
 {
-    printf("[WIFI] WiFi information!\n");
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
+    while (1) {
+        printf("[WIFI] WiFi information!\n");
+        esp_chip_info_t chip_info;
+        esp_chip_info(&chip_info);
 
-    /* Print chip WiFi information */
-    printf("[WIFI] Chip %s con %d núcleos\n", CONFIG_IDF_TARGET, chip_info.cores);
+        /* Print chip WiFi information */
+        printf("[WIFI] Chip %s con %d núcleos\n", CONFIG_IDF_TARGET, chip_info.cores);
 
-    if (chip_info.features & CHIP_FEATURE_WIFI_BGN) {
-        printf("[WIFI] ✅ Este SoC tiene WiFi.\n");
-        print_wifi_info();
-    } else {
-        printf("[WIFI] ℹ️ Este SoC no tiene capacidades WiFi.\n");
+        if (chip_info.features & CHIP_FEATURE_WIFI_BGN) {
+            printf("[WIFI] ✅ Este SoC tiene WiFi.\n");
+            print_wifi_info();
+        } else {
+            printf("[WIFI] ℹ️ Este SoC no tiene capacidades WiFi.\n");
+        }
+        
+        vTaskDelay(CONFIG_WIFI_TASK_DELAY / portTICK_PERIOD_MS);
     }
     
-    for (int i = 13; i >= 0; i--) {
-        printf("[WIFI] Restarting in %d seconds...\n", i);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-    printf("[WIFI] Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
 
 void app_main(void)
